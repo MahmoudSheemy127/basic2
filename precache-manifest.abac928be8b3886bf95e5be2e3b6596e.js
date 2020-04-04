25f6ddc7e4952ff15ca5864dc4d7f5232705ab4d
MahmoudSheemy127 <mahmoudalshemy.3@gmail.com>
2020-04-05 01:22:32 +0200

    Updates

---
+++ precache-manifest.abac928be8b3886bf95e5be2e3b6596e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ac015e03c61a0ea9e5a71e64d0e20f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f4c9362b57110301a7a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fc72b77836b9b2e1db7",
+    "url": "/static/js/2.44169012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44169012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4c9362b57110301a7a",
+    "url": "/static/js/main.0e29d856.chunk.js"
+  },
+  {
+    "revision": "eaa69f93121abe5efedc",
+    "url": "/static/js/runtime-main.ffdb9039.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
